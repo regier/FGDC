786d978700190081a00bd6fa7b1add4ed17bf4c3
Regier Kunkel <regier.kunkel@gmail.com>
2021-12-03 19:41:56 +0000

    Add and set cmake flags for each component. Minor tweaks.

--- Compile.sh
+++ Compile.sh
@@ -32,22 +32,26 @@ say () {
 # Makes script cache compiler output using cmake if cmake is available.
 export PATH="/usr/lib/ccache:${PATH}"
 
-# Compiler options
-compiler_flags="-march=native -mtune=native" # Set compiler flags here.
+# Compiler options.
+compiler_flags="-w -march=native -mtune=native -pipe" # Set compiler flags here.
 export CFLAGS="$compiler_flags"
 export CXXFLAGS="$compiler_flags"
 
-# Compile function.
+# build function.
+# This function will compile the source code for each component.
+# Using the number of logical CPUs as the amount of simultaneous compile jobs.
 build () {
   make -j $(nproc)
   make install
 }
 
 # cmake function.
+# For OSG, SG and FG, this function will run cmake to set build options.
+# So then after we can build the source code with the options we want.
 cmaking () {
   message="Compiling and Installing ""$component" say
   mkdir -p "$compiling_directory"/"$component" && cd "$compiling_directory"/"$component"
-  cmake "$download_directory"/"$component" -DCMAKE_CXX_FLAGS="$compiler_flags" -DCMAKE_C_FLAGS="$compiler_flags" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$install_directory"
+  cmake "$download_directory"/"$component" -DCMAKE_CXX_FLAGS="$compiler_flags" -DCMAKE_C_FLAGS="$compiler_flags" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$install_directory" "$cmake_flags"
 }
 
 # PLIB
@@ -59,15 +63,18 @@ build
 
 # OSG
 component="OSG"
+export cmake_flags="-DBUILD_OSG_APPLICATIONS=OFF"
 cmaking
 build
 
 # SG
 component="SG"
+export cmake_flags="-DENABLE_SIMD_CODE=ON -DBUILD_TESTING=OFF -DENABLE_TESTS=OFF"
 cmaking
 build
 
 # FG
 component="FG"
+export cmake_flags="-DFG_BUILD_TYPE=Release -DBUILD_TESTING=OFF"
 cmaking
 build
